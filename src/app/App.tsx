898cf72a1887ed7495d18d546e2a5c77d628b1be
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-06-08 00:39:40 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
-import WebsiteBuilder from './pages/WebsiteBuilder';
+import EnhancedWebsiteBuilder from './pages/EnhancedWebsiteBuilder';
 import CustomRequest from './pages/CustomRequest';
 import PaymentSuccess from './pages/PaymentSuccess';
-import EnhancedWebsiteBuilder from './pages/EnhancedWebsiteBuilder';
 
-// In routes:
-<Route path="/website-builder" element={<EnhancedWebsiteBuilder />} />
 export default function App() {
   return (
     <Router>
@@ -15,7 +12,7 @@ export default function App() {
         {/* Client-facing routes only - Admin is a separate deployment */}
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/website-builder" element={<WebsiteBuilder />} />
+        <Route path="/website-builder" element={<EnhancedWebsiteBuilder />} />
         <Route path="/custom-request" element={<CustomRequest />} />
         <Route path="/payment-success" element={<PaymentSuccess />} />
       </Routes>
